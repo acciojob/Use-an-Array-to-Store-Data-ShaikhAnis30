590eee166076474084e5fc9f50a5189b327e12df
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-18 19:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+let yourArray = [
+  [
+    {
+      one: "1",
+      two: 2,
+	  res: true
+    },
+    {
+      three: "3",
+      four: 4,
+	  res: false
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: 5,
+	  res: true
+    },
+    {
+      c: "c",
+      d: 8,
+	  res: false
+    }
+  ]
+];
